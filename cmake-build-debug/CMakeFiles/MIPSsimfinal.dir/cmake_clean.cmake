7eaaf4c11bbd477deb3e502f36a935c30a2ee7a1
SkullFang <248482914@qq.com>
2017-06-25 23:30:33 +0800

    mips

---
+++ cmake-build-debug/CMakeFiles/MIPSsimfinal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MIPSsimfinal.dir/main.cpp.o"
+  "MIPSsimfinal.pdb"
+  "MIPSsimfinal"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MIPSsimfinal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
